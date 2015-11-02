5d7cd70d9491c62ca4778a63895df5861cf4a21a
yoh <yoh@jp.fujitsu.com>
2015-11-02 14:34:00 +0900

    Revert "fix enable/disable problem of paramPage"
    This reverts commit 1339b098b8aae5ea24bee8f6f8d0be677875e0cd.

--- lib/python/pdi_paramPage.py
+++ lib/python/pdi_paramPage.py
@@ -337,9 +337,6 @@ class ParamPage(wx.ScrolledWindow):
                 ctlx.Enable(p.useExcept and not p.disable)
 
             continue # end of for(p)
-
-        # done
-        self.Refresh()
         return True
 
     # event handlers
